951f6c0380f4c9e4117d94f5738e156c3cd58304
Jhon Marlon Florez <jmarloncin07@gmail.com>
2026-08-02 14:39:48 -0500

    feat: configured routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,12 @@
-/**
- * configura la aplicación Express y el uso de las rutas
- */
+import express from "express";
+import cors from "cors";
+import songRoutes from "./routes/songRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", songRoutes);
+
+export default app;
