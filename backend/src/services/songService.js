581a4e0bd80b79e2f7399b71549dbaff62e95576
Jhon Marlon Florez <jmarloncin07@gmail.com>
2026-08-02 12:49:14 -0500

    feat: CRUD operations added

--- backend/src/services/songService.js
+++ backend/src/services/songService.js
@@ -29,3 +29,61 @@ export async function createSong(songData) {
     return response.data;
 }
 
+export async function getAllSongs() {
+    const response = await supabase.from("songs").select("*");
+
+    if (response.error) {
+        throw new Error(response.error.message);
+    }
+
+    return response.data;
+}
+
+export async function getSongById(id) {
+
+    const response = await supabase.from("songs")
+    .select()
+    .eq("id", id)
+    .single();
+
+    if (response.error) {
+        throw new Error(response.error.message);
+    }
+
+    return response.data;
+}
+
+export async function updateSong(id, songData) {
+    if (!songData.title || !songData.author || !songData.content) {
+        // Detiene la ejecución si ocurre un error y luego envía ese error para que se atrape en un catch
+        throw new Error("To update the song, one of these parameters cannot be empty: title, author, and content");
+    }
+
+    const response = await supabase.from("songs")
+        .update({
+            title: songData.title,
+            author: songData.author,
+            chorus: songData.chorus,
+            content: songData.content
+        })
+        .eq("id", id)
+        .select()
+        .single();
+
+    if (response.error) {
+        throw new Error(response.error.message);  
+    }
+
+    return response.data;
+}
+
+export async function deleteSong(id) {
+    
+    const error = await supabase.from("songs").delete().eq("id", id);
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return { success: true, message: "Song deleted successfully" };
+}
